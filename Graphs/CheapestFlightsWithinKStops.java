391bc02a82e0a04aaa2f85b1f310dde7cd8ce1d2
Sahil Mishra <106021283+sahilmishra03@users.noreply.github.com>
2026-04-14 19:51:44 +0530

    14-04-26

--- Graphs/CheapestFlightsWithinKStops.java
+++ Graphs/CheapestFlightsWithinKStops.java
@@ -1,62 +1,65 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 class Solution {
-    public class Triplet {
-        int node,
-                dist,
-                stops;
+    class Triplet {
+        int node, cost, stops;
 
-        Triplet(int node, int dist, int stops) {
+        Triplet(int node, int cost, int stops) {
             this.node = node;
-            this.dist = dist;
+            this.cost = cost;
             this.stops = stops;
         }
+    }
 
+    public class Pair {
+        int node, cost;
+
+        Pair(int node, int cost) {
+            this.node = node;
+            this.cost = cost;
+        }
     }
 
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        int[] res = new int[n];
+        PriorityQueue<Triplet> pq = new PriorityQueue<>((a, b) -> a.stops - b.stops);
 
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
 
         for (int i = 0; i < flights.length; i++) {
-            adj.get(flights[i][0]).add(new int[] { flights[i][1], flights[i][2] });
+            adj.get(flights[i][0]).add(new Pair(flights[i][1], flights[i][2]));
         }
 
-        PriorityQueue<Triplet> q = new PriorityQueue<>((a, b) -> a.stops - b.stops);
-        q.add(new Triplet(src, 0, 0));
-
-        int[] res = new int[n];
-
-        for (int i = 0; i < res.length; i++) {
+        for (int i = 0; i < n; i++) {
             res[i] = Integer.MAX_VALUE;
         }
-        res[src] = 0;
-        while (!q.isEmpty()) {
-            Triplet front = q.remove();
 
+        pq.add(new Triplet(src, 0, 0));
+        res[src] = 0;
+
+        while (!pq.isEmpty()) {
+            Triplet front = pq.remove();
             if (front.stops > k) {
                 continue;
             }
 
-            if (res[front.node] > front.dist) {
-                continue;
-            }
-
             for (int i = 0; i < adj.get(front.node).size(); i++) {
-                int cal = adj.get(front.node).get(i)[1] + front.dist;
+                Pair val = adj.get(front.node).get(i);
+                int cal = front.cost + val.cost;
 
-                if (cal < res[adj.get(front.node).get(i)[0]]) {
-                    res[adj.get(front.node).get(i)[0]] = cal;
-                    q.add(new Triplet(adj.get(front.node).get(i)[0], cal, front.stops + 1));
+                if (res[val.node] > cal) {
+                    res[val.node] = cal;
+                    pq.add(new Triplet(val.node, cal, front.stops + 1));
                 }
             }
         }
-        if (res[dst] == Integer.MAX_VALUE)
-            return -1;
+
         return res[dst];
     }
 }
--- Graphs/PathWithMinimumEffort.java
+++ Graphs/PathWithMinimumEffort.java
@@ -1,10 +1,10 @@
 import java.util.PriorityQueue;
 
 class Solution {
-    public class Pair {
+    class Triplet {
         int i, j, efforts;
 
-        Pair(int i, int j, int efforts) {
+        Triplet(int i, int j, int efforts) {
             this.i = i;
             this.j = j;
             this.efforts = efforts;
@@ -14,60 +14,60 @@ class Solution {
     public int minimumEffortPath(int[][] heights) {
         int n = heights.length;
         int m = heights[0].length;
+        PriorityQueue<Triplet> pq = new PriorityQueue<>((a, b) -> a.efforts - b.efforts);
         int[][] res = new int[n][m];
-        for (int i = 0; i < res.length; i++) {
-            for (int j = 0; j < res[i].length; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
                 res[i][j] = Integer.MAX_VALUE;
             }
         }
+        pq.add(new Triplet(0, 0, 0));
 
-        PriorityQueue<Pair> q = new PriorityQueue<>((a, b) -> a.efforts - b.efforts);
-        q.add(new Pair(0, 0, 0));
-        res[0][0] = 0;
+        while (!pq.isEmpty()) {
+            Triplet front = pq.remove();
 
-        while (!q.isEmpty()) {
-            Pair front = q.remove();
-            if (res[front.i][front.j] < front.efforts) {
-                continue;
-            }
             // Up
             if (front.i - 1 >= 0) {
-                int efforts = Math.abs(heights[front.i][front.j] - heights[front.i - 1][front.j]);
-                int maxEffort = Math.max(front.efforts, efforts);
-                if (maxEffort < res[front.i - 1][front.j]) {
-                    res[front.i - 1][front.j] = maxEffort;
-                    q.add(new Pair(front.i - 1, front.j, maxEffort));
+                int cal = Math.abs(heights[front.i - 1][front.j] - heights[front.i][front.j]);
+                int maxEfforts = Math.max(front.efforts, cal);
+
+                if (res[front.i - 1][front.j] > maxEfforts) {
+                    res[front.i - 1][front.j] = maxEfforts;
+                    pq.add(new Triplet(front.i - 1, front.j, maxEfforts));
                 }
             }
             // Down
             if (front.i + 1 < n) {
-                int efforts = Math.abs(heights[front.i][front.j] - heights[front.i + 1][front.j]);
-                int maxEffort = Math.max(front.efforts, efforts);
-                if (maxEffort < res[front.i + 1][front.j]) {
-                    res[front.i + 1][front.j] = maxEffort;
-                    q.add(new Pair(front.i + 1, front.j, maxEffort));
+                int cal = Math.abs(heights[front.i + 1][front.j] - heights[front.i][front.j]);
+                int maxEfforts = Math.max(front.efforts, cal);
+
+                if (res[front.i + 1][front.j] > maxEfforts) {
+                    res[front.i + 1][front.j] = maxEfforts;
+                    pq.add(new Triplet(front.i + 1, front.j, maxEfforts));
                 }
             }
             // Left
             if (front.j - 1 >= 0) {
-                int efforts = Math.abs(heights[front.i][front.j] - heights[front.i][front.j - 1]);
-                int maxEffort = Math.max(front.efforts, efforts);
-                if (maxEffort < res[front.i][front.j - 1]) {
-                    res[front.i][front.j - 1] = maxEffort;
-                    q.add(new Pair(front.i, front.j - 1, maxEffort));
+                int cal = Math.abs(heights[front.i][front.j - 1] - heights[front.i][front.j]);
+                int maxEfforts = Math.max(front.efforts, cal);
+
+                if (res[front.i][front.j - 1] > maxEfforts) {
+                    res[front.i][front.j - 1] = maxEfforts;
+                    pq.add(new Triplet(front.i, front.j - 1, maxEfforts));
                 }
             }
             // Right
             if (front.j + 1 < m) {
-                int efforts = Math.abs(heights[front.i][front.j] - heights[front.i][front.j + 1]);
-                int maxEffort = Math.max(front.efforts, efforts);
-                if (maxEffort < res[front.i][front.j + 1]) {
-                    res[front.i][front.j + 1] = maxEffort;
-                    q.add(new Pair(front.i, front.j + 1, maxEffort));
+                int cal = Math.abs(heights[front.i][front.j + 1] - heights[front.i][front.j]);
+                int maxEfforts = Math.max(front.efforts, cal);
+
+                if (res[front.i][front.j + 1] > maxEfforts) {
+                    res[front.i][front.j + 1] = maxEfforts;
+                    pq.add(new Triplet(front.i, front.j + 1, maxEfforts));
                 }
             }
         }
 
-        return res[n - 1][m - 1];
+        return res[n-1][m-1];
     }
 }
