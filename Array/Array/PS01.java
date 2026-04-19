a83a000b7fbbb903e03cf83339e90b8923414f16
Sahil Mishra <106021283+sahilmishra03@users.noreply.github.com>
2026-04-19 19:49:02 +0530

    19-04-26

--- Array/Array/PS01.java
+++ Array/Array/PS01.java
@@ -2,126 +2,126 @@ package Array;
 
 public class PS01 {
     public static void main(String[] args) {
-//        int[] firstArr = {1, 2, 3, 4, 5};
-//        int[] secondArr = new int[firstArr.length];
-//        for (int i = 0; i < firstArr.length; i++) {
-//            secondArr[i] = firstArr[firstArr.length - 1 - i];
-//        }
-//        for (int x = 0; x < secondArr.length; x++) {
-//            System.out.println(secondArr[x]);
-//        }
-//        int[] firstArr = {1, 2, 3, 4, 5};
-//        int max =firstArr[0];
-//        for (int i =1;i<firstArr.length;i++){
-//            if (max>firstArr[i]){
-//
-//            }else {
-//                max=firstArr[i];
-//            }
-//        }
-//        System.out.println(max);
-//        int[] firstArr = {7, 2, 3, 6, 1};
-//        int min =firstArr[0];
-//        for (int i =1;i<firstArr.length;i++){
-//            if (min<firstArr[i]){
-//
-//            }else {
-//                min=firstArr[i];
-//            }
-//        }
-//        System.out.println(min);
-//        int arr[] = {1, 2, 7, 5, 6, 8};
-//        int max = arr[0];
-//        int secmax = Integer.MIN_VALUE;
-//        int i = 1;
-//
-//        while (i < arr.length) {
-//            if (arr[i] > max) {
-//                secmax = max;
-//                max = arr[i];
-//            } else if (arr[i] > secmax) {
-//                secmax = arr[i];
-//            }
-//            i++;
-//        }
-//
-//        System.out.println(max);
-//        System.out.println(secmax);
-//        int arr2[] = {1, 2, 7, 5, 6, 8};
-//        int min = arr[0];
-//        int secmin = Integer.MAX_VALUE;
-//        int J = 1;
-//
-//        while (J < arr.length) {
-//            if (arr[J] < min) {
-//                secmin = min;
-//                min = arr[J];
-//            } else if (arr[J] <secmin) {
-//                secmin = arr[J];
-//            }
-//            J++;
-//        }
-//
-//        System.out.println(min);
-//        System.out.println(secmin);
-//        int Arrays[] = {1,1,2,5,4,3,1,1,5,1,1,1,2,1,6,1};
-//        int count=0;
-//        int num=1;
-//        for (int i = 0;i< Arrays.length;i++){
-//            if (num==Arrays[i]){
-//                count=count+1;
-//            }
-//            else {
-//
-//            }
-//        }
-//        System.out.println(count);
-//        int Arrays[] = {1, 1, 2, 5, 4, 3, 1, 1, 5, 1, 1, 1, 2, 1, 6, 1};
-//        int number = 5;
-//        for (int i = 0; i < Arrays.length; i++) {
-//            if (number == Arrays[i]) {
-//                System.out.println("Number is found at " + i + " index");
-//            } else {
-//
-//            }
-//        int Arrays[] = {1, 1, 2, 5, 4, 3, 1, 1, 5, 1, 1, 1, 2, 1, 6, 1, 0, 4, 6, 1};
-//        int number1 = 0;
-//        int number2 = 1;
-//        for (int i = 0; i < Arrays.length; i++) {
-//            if (number1 == Arrays[i]) {
-//                System.out.println("Number " + number1 + " is found at " + i + " index");
-//            } else if (number2 == Arrays[i]) {
-//                System.out.println("Number " + number2 + " is found at " + i + " index");
-//            }
-//        }
-//        int[] arr = {0, 1, 0, 0, 2, 1, 2};
-//        int temp;
-//
-//        for (int i = 0; i < arr.length - 1; i++) {
-//            for (int j = i + 1; j < arr.length; j++) {
-//                if (arr[i] > arr[j]) {
-//                    temp = arr[j];
-//                    arr[j] = arr[i];
-//                    arr[i] = temp;
-//                }
-//            }
-//        }
-//        for (int x = 0; x < arr.length; x++) {
-//            System.out.println(arr[x]);
-//        }
-        // int[] arr1 = {1, 2, 3, 4, 5};
-//        int[] arr2 = new int[arr1.length];
-//        for (int i = arr1.length - 1; i >= 0; i--) {
-//            arr2[arr1.length - i - 1] = arr1[i];
-//        }
-//        for (int x = 0; x < arr2.length; x++) {
-//            System.out.println(arr2[x]);
-//        }
-//        int sum = 0;
-//        for (int i = arr1.length-1;i>=0 ;i-- ){
-//            sum = sum+arr1[i];
-//        }
-//        int avg = sum/ arr1.length;
-//        System.out.println(avg);
+    //    int[] firstArr = {1, 2, 3, 4, 5};
+    //    int[] secondArr = new int[firstArr.length];
+    //    for (int i = 0; i < firstArr.length; i++) {
+    //        secondArr[i] = firstArr[firstArr.length - 1 - i];
+    //    }
+    //    for (int x = 0; x < secondArr.length; x++) {
+    //        System.out.println(secondArr[x]);
+    //    }
+    // //    int[] firstArr = {1, 2, 3, 4, 5};
+    //    int max =firstArr[0];
+    //    for (int i =1;i<firstArr.length;i++){
+    //        if (max>firstArr[i]){
+
+    //        }else {
+    //            max=firstArr[i];
+    //        }
+    //    }
+    //    System.out.println(max);
+    // //    int[] firstArr = {7, 2, 3, 6, 1};
+    //    int min =firstArr[0];
+    //    for (int i =1;i<firstArr.length;i++){
+    //        if (min<firstArr[i]){
+
+    //        }else {
+    //            min=firstArr[i];
+    //        }
+    //    }
+    //    System.out.println(min);
+    //    int arr[] = {1, 2, 7, 5, 6, 8};
+    // //    int max = arr[0];
+    //    int secmax = Integer.MIN_VALUE;
+    //    int i = 1;
+
+    //    while (i < arr.length) {
+    //        if (arr[i] > max) {
+    //            secmax = max;
+    //            max = arr[i];
+    //        } else if (arr[i] > secmax) {
+    //            secmax = arr[i];
+    //        }
+    //        i++;
+    //    }
+
+    //    System.out.println(max);
+    //    System.out.println(secmax);
+    //    int arr2[] = {1, 2, 7, 5, 6, 8};
+    // //    int min = arr[0];
+    //    int secmin = Integer.MAX_VALUE;
+    //    int J = 1;
+
+    //    while (J < arr.length) {
+    //        if (arr[J] < min) {
+    //            secmin = min;
+    //            min = arr[J];
+    //        } else if (arr[J] <secmin) {
+    //            secmin = arr[J];
+    //        }
+    //        J++;
+    //    }
+
+    //    System.out.println(min);
+    //    System.out.println(secmin);
+    //    int Arrays[] = {1,1,2,5,4,3,1,1,5,1,1,1,2,1,6,1};
+    //    int count=0;
+    //    int num=1;
+    //    for (int i = 0;i< Arrays.length;i++){
+    //        if (num==Arrays[i]){
+    //            count=count+1;
+    //        }
+    //        else {
+
+    //        }
+    //    }
+    //    System.out.println(count);
+    // //    int Arrays[] = {1, 1, 2, 5, 4, 3, 1, 1, 5, 1, 1, 1, 2, 1, 6, 1};
+    //    int number = 5;
+    //    for (int i = 0; i < Arrays.length; i++) {
+    //        if (number == Arrays[i]) {
+    //            System.out.println("Number is found at " + i + " index");
+    //        } else {
+
+    //        }
+    // //    int Arrays[] = {1, 1, 2, 5, 4, 3, 1, 1, 5, 1, 1, 1, 2, 1, 6, 1, 0, 4, 6, 1};
+    //    int number1 = 0;
+    //    int number2 = 1;
+    //    for (int i = 0; i < Arrays.length; i++) {
+    //        if (number1 == Arrays[i]) {
+    //            System.out.println("Number " + number1 + " is found at " + i + " index");
+    //        } else if (number2 == Arrays[i]) {
+    //            System.out.println("Number " + number2 + " is found at " + i + " index");
+    //        }
+    //    }
+    // //    int[] arr = {0, 1, 0, 0, 2, 1, 2};
+    //    int temp;
+
+    //    for (int i = 0; i < arr.length - 1; i++) {
+    //        for (int j = i + 1; j < arr.length; j++) {
+    //            if (arr[i] > arr[j]) {
+    //                temp = arr[j];
+    //                arr[j] = arr[i];
+    //                arr[i] = temp;
+    //            }
+    //        }
+    //    }
+    //    for (int x = 0; x < arr.length; x++) {
+    //        System.out.println(arr[x]);
+    //    }
+    //     int[] arr1 = {1, 2, 3, 4, 5};
+    // //    int[] arr2 = new int[arr1.length];
+    //    for (int i = arr1.length - 1; i >= 0; i--) {
+    //        arr2[arr1.length - i - 1] = arr1[i];
+    //    }
+    //    for (int x = 0; x < arr2.length; x++) {
+    //        System.out.println(arr2[x]);
+    //    }
+    //    int sum = 0;
+    //    for (int i = arr1.length-1;i>=0 ;i-- ){
+    //        sum = sum+arr1[i];
+    //    }
+    //    int avg = sum/ arr1.length;
+    //    System.out.println(avg);
     }
 }
