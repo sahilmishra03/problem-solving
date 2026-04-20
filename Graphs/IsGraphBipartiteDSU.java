9155f306d943e2232141de0da57102ac2eb1c2fd
Sahil Mishra <106021283+sahilmishra03@users.noreply.github.com>
2026-04-20 22:59:07 +0530

    20-04-26

---
+++ Graphs/IsGraphBipartiteDSU.java
@@ -0,0 +1,56 @@
+class Solution {
+    int parent[];
+    int size[];
+    boolean parity[];
+
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        parent = new int[n + 1];
+        size = new int[n + 1];
+        parity = new boolean[n + 1];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+            size[i] = 1;
+            parity[i] = false;
+        }
+
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph.length; j++) {
+                if (graph[i][j] > i) {
+                    if (find(i) == find(graph[i][j]) && parity[i] == parity[graph[i][j]]) {
+                        return false;
+                    } else {
+                        union(i, graph[i][j]);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void union(int i, int j) {
+        int leaderA = find(i);
+        int leaderB = find(j);
+
+        if (leaderA != leaderB) {
+            if (size[leaderA] > size[leaderB]) {
+                parent[leaderB] = leaderA;
+                size[leaderA] += size[leaderB];
+                parity[j] = !parity[i];
+            } else {
+                parent[leaderA] = leaderB;
+                size[leaderB] += size[leaderA];
+                parity[i] = !parity[j];
+            }
+        }
+    }
+
+    private int find(int i) {
+        if (parent[i] == i) {
+            return i;
+        }
+
+        return parent[i] = find(parent[i]);
+    }
+}
---
+++ Graphs/KruskalsAlgorithm.java
@@ -0,0 +1,70 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public class Edge {
+        int u, v, wt;
+
+        Edge(int u, int v, int wt) {
+            this.u = u;
+            this.v = v;
+            this.wt = wt;
+        }
+    }
+
+    int parent[];
+    int size[];
+
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        parent = new int[n + 1];
+        size = new int[n + 1];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.wt - b.wt);
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                int x1 = points[i][0], y1 = points[i][1],
+                        x2 = points[j][0], y2 = points[j][1];
+
+                int wt = Math.abs(x1 - x2) + Math.abs(y2 - y1);
+
+                pq.add(new Edge(i, j, wt));
+            }
+        }
+        int minCost = 0;
+        while (!pq.isEmpty()) {
+            Edge front = pq.remove();
+            if (find(front.u) != find(front.v)) {
+                minCost += front.wt;
+                union(front.u, front.v);
+            }
+        }
+
+        return minCost;
+    }
+
+    public void union(int i, int j) {
+        int leaderA = find(i);
+        int leaderB = find(j);
+
+        if (leaderA != leaderB) {
+            if (size[leaderA] > size[leaderB]) {
+                parent[leaderB] = leaderA;
+                size[leaderA] += size[leaderB];
+            } else {
+                parent[leaderA] = leaderB;
+                size[leaderB] += size[leaderA];
+            }
+        }
+    }
+
+    private int find(int i) {
+        if (parent[i] == i) {
+            return i;
+        }
+
+        return parent[i] = find(parent[i]);
+    }
+}
